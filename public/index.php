96c79eced8d4e7b86df3b6a2854cd9eac7dd513a
Gonjs <vanthien7027@gmail.com>
2025-09-11 20:18:27 +0700

    first commit

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shoe Shop</title>
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+    <h1 class="text-4xl font-bold text-center text-blue-600 mt-10">
+        Dự án Shoe Shop đã sẵn sàng!
+    </h1>
+</body>
+</html>
---
+++ src/models/repositories/database.php
@@ -0,0 +1,15 @@
+<?php 
+    $host = "localhost";
+    $dbname="shoe_shop_db";
+    $username = "root";
+    $password = "";
+    try {
+        
+        $conn=new PDO("mysql:host=$host;dbname=$dbname;charset=utf8",$username,$password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        echo "Kết nối thành công";
+    } catch (PDOException $e) {
+        //throw $th;
+        die("Kết nối thất bại: ".$e->getMessage());
+    }
+?>
