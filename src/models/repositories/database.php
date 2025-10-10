bfa146413c9420d76fed4d6dfe6055a6e2b9727e
Gonjs <vanthien7027@gmail.com>
2025-10-10 16:07:56 +0700

    FIX: ENV v2

--- src/models/repositories/database.php
+++ src/models/repositories/database.php
@@ -1,9 +1,13 @@
 <?php
-$host = "localhost";
-$port = 1133;
-$dbname = "shoe_shop_db";
-$username = "root";
-$password = "";
+require_once __DIR__ . '/../../../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../../');
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$port = $_ENV['DB_PORT'];
+$dbname = $_ENV['DB_DATABASE'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
 try {
 
     $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
